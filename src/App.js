1c9d9185e9bbcceef683445dccd8077bf69e69f0
Emmanuel <emmanuelchinaza5777@gmail.com>
2021-02-07 16:40:38 +0100

    CV first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import CoverPage from "./Compoents/pages/CoverPage";
+import { Route, Switch } from "react-router-dom";
+import CVPage from "./Compoents/pages/CVPage";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path="/" component={CoverPage} />
+        <Route path="/cv-details" component={CVPage} />
+      </Switch>
     </div>
   );
 }
---
+++ src/Compoents/pages/CoverPage.jsx
@@ -0,0 +1,19 @@
+import "../styles/CoverPage.css";
+import { Link } from "react-router-dom";
+
+
+const CoverPage = (props) => {
+    return (
+        <div className="coverpage">
+            <section className="content">
+                <h2 className="animate__animated animate__heartBeat">This is Chukwunazaekperes' online CV.</h2>
+
+                <Link to="/cv-details" className="exp__btn">
+                    CV details
+                </Link>
+            </section>
+        </div>
+    );
+}
+
+export default CoverPage;
